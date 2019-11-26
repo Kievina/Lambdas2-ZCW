913f4f72e7f93434677ab4f4ec3ee3cfda033b1c
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-26 17:08:28 -0500

    person class created

---
+++ main/java/Person.java
@@ -0,0 +1,73 @@
+package java;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public class Person {
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+//        this.roster = new ArrayList<>();
+    }
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    private String name;
+    private LocalDate birthday;
+    private Sex gender;
+    private String emailAddress;
+//    private ArrayList<Person> roster;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getAge() {
+        return Period.between(birthday, LocalDate.now()).getYears();
+    }
+
+    public void printPerson() {
+        System.out.println("Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", gender=" + gender +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}');
+    }
+}
+
+
