7dd9884df4407ae90a072a3d2b01eca58f020e49
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-27 07:27:23 -0500

    use lambda expressions

--- lambdalab/main/java/LambdaExpression.java
+++ lambdalab/main/java/LambdaExpression.java
@@ -2,7 +2,6 @@
 import java.util.List;
 
 public class LambdaExpression {
-    List<Person> personList1 = Person.createRoster();
 
     public static void printPersons(List<Person> roster, CheckPerson tester) {
         for (Person p : roster) {
@@ -12,32 +11,15 @@ public class LambdaExpression {
         }
     }
 
-    public void createClass1() {
+    public static void main(String[] args) {
+        List<Person> personList1 = Person.createRoster();
 
-        CheckPerson tester1 = new CheckPerson() {
-            @Override
-            public Boolean test(Person p) {
-                return p.getAge() > 20;
-            }
-        };
+        CheckPerson tester1 = p -> p.getAge() > 20;
         System.out.println("Persons who are older than 20 years:");
         printPersons(personList1, tester1);
-    }
 
-    public void createClass2() {
-        CheckPerson tester2 = new CheckPerson() {
-            @Override
-            public Boolean test(Person p) {
-                return p.getAge() < 19;
-            }
-        };
+        CheckPerson tester2 = p -> p.getAge() < 19;
         System.out.println("Persons who are younger than 19 years:");
         printPersons(personList1, tester2);
-    }
-
-    public static void main(String[] args) {
-        LambdaExpression an = new LambdaExpression();
-        an.createClass1();
-        an.createClass2();
     }
 }
